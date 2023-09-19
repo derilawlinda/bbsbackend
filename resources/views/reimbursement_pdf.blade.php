a2b9c1c3dfd353a07e4104e3e984003addd8aff2
Deril Alwinda <deril.alwinda@harrisma.com>
2023-09-19 21:52:53 +0700

    add amount in reimbursement

--- resources/views/reimbursement_pdf.blade.php
+++ resources/views/reimbursement_pdf.blade.php
@@ -100,7 +100,7 @@
 
             <tr>
                 <td>Amount</td>
-                <td>{{$reimbursement["U_TotalAmount"]}}</td>
+                <td>Rp {{ number_format( $account["U_TotalAmount"] , 2 , '.' , ',' )}}</td>
 
                 <td>Budget</td>
                 <td>{{$reimbursement["U_BudgetCode"]}} - {{$reimbursement["BudgetName"]}}</td>
